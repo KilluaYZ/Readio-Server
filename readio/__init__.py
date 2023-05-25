1d8a335d7720a564ed5aab6321b9eba23ba833a1
ziyang <2959243019@qq.com>
2023-05-25 20:59:11 +0800

    添加pieces收藏功能

--- readio/__init__.py
+++ readio/__init__.py
@@ -12,7 +12,7 @@ from readio.auth import appAuth
 from readio.database.init_db import init_db
 from readio.mainpage import appHomePage, appBookShelfPage, appBookDetailsPage
 from readio.monitor.monitor import monitor
-from readio.manage import fileManage, worksManage, userManage, likesManage
+from readio.manage import fileManage, worksManage, userManage
 from readio.utils.json import CustomJSONEncoder
 
 
@@ -44,7 +44,6 @@ def create_app():
     app.register_blueprint(appBookDetailsPage.bp)
     app.register_blueprint(fileManage.bp)
     app.register_blueprint(worksManage.bp)
-    app.register_blueprint(likesManage.bp)
 
     # 配置定时任务
     # 该任务作用是每个一个小时检查一次user_token表，将超过1天未活动的token删掉（随便定的，后面改
--- readio/manage/worksManage.py
+++ readio/manage/worksManage.py
@@ -15,7 +15,6 @@ import readio.database.connectPool
 import readio.utils.check as check
 from readio.utils.myExceptions import *
 from readio.utils.executeSQL import *
-from readio.manage.likesManage import __get_all_like_pieces_id_by_userid
 
 # appAuth = Blueprint('/auth/app', __name__)
 bp = Blueprint('worksManage', __name__, url_prefix='/works')
@@ -26,7 +25,7 @@ pooldb = readio.database.connectPool.pooldb
 def __random_get_pieces_brief_sql(size: int) -> list:
     try:
         conn, cursor = pooldb.get_conn()
-        cursor.execute('select piecesId, seriesId, title, userId, status, content, likes, views, shares from pieces')
+        cursor.execute('select piecesId, seriesId, title, userId, status, content, collect, likes, views, shares from pieces')
         rows = cursor.fetchall()
         # 随机从列表中抽取size个元素
         if size > len(rows):
@@ -170,24 +169,31 @@ def get_bref():
         for i in range(len(rows)):
             rows[i]['content'] = rows[i]['content'][:40]
 
-        # 获取用户点赞情况
-        # 先设置未点赞
+        # 获取用户点赞收藏情况
+        # 先设置未点赞，未收藏
         for i in range(len(rows)):
             rows[i]["isLiked"] = 0
+            rows[i]["isCollected"] = 0
         try:
-            # print(f'[DEBUG] enter try')
+            print(f'[DEBUG] enter try')
             # 尝试获取user点赞信息
             user = check_user_before_request(request)
             userId = user['id']
             user_all_liked_pieces_id_list = __get_all_like_pieces_id_by_userid(userId)
+            user_all_collected_pieces_id_list = __get_all_collect_pieces_id_by_userid(userId)
             # print(f'[DEBUG] user_all_liked_pieces_id_list = {user_all_liked_pieces_id_list}')
+            # print(f'[DEBUG] user_all_collected_pieces_id_list = {user_all_collected_pieces_id_list}')
             for i in range(len(rows)):
                 if int(rows[i]['piecesId']) in user_all_liked_pieces_id_list:
                     # print(f'[DEBUG] enter like')
                     # 在用户喜欢的列表里
                     rows[i]["isLiked"] = 1
+                if int(rows[i]['piecesId']) in user_all_collected_pieces_id_list:
+                    # print(f'[DEBUG] enter collect')
+                    # 在用户喜欢的列表里
+                    rows[i]["isCollected"] = 1
         except NetworkException:
-            pass
+            print("用户未登录或不存在，不返回点赞收藏信息")
         except Exception as e:
             raise e
 
@@ -997,3 +1003,239 @@ def add_tag_series_relation():
         return build_error_response(code=500, msg='服务器内部错误')
 
 
+# 喜欢收藏等零碎的东西
+
+def __add_pieces_like(userId, piecesId, trans=None):
+    sql = 'insert into user_pieces_like(userId, piecesId) values(%s, %s)'
+    if trans is None:
+        return execute_sql_write(pooldb, sql, (userId, piecesId))
+    else:
+        return trans.execute(sql, (userId, piecesId))
+
+
+def __get_all_like_pieces_id_by_userid(userId) -> List[Dict]:
+    """
+    通过用户Id来获取该用户所有like的pieces的id
+    """
+    sql = 'select piecesId from user_pieces_like where userId=%s'
+    rows = execute_sql_query(pooldb, sql, userId)
+    rows = list(map(lambda x: int(x['piecesId']), rows))
+    return rows
+
+
+def __get_all_like_pieces_obj_by_userid(userId) -> List[Dict]:
+    """
+    通过用户Id来获取该用户所有like的pieces
+    """
+    sql = 'select pieces.piecesId as piecesId, pieces.seriesId as seriesId, ' \
+          'pieces.title as title, pieces.userId as userId, pieces.content as content, ' \
+          'pieces.createTime as createTime, pieces.updateTime as updateTime, ' \
+          'pieces.status as status, pieces.likes as likes, pieces.views as views, ' \
+          'pieces.shares as shares, pieces.collect as collect from pieces, user_pieces_like ' \
+          'where pieces.piecesId = user_pieces_like.piecesId ' \
+          'and user_pieces_like.userId=%s'
+    return execute_sql_query(pooldb, sql, userId)
+
+
+def __check_if_user_like_pieces(userId, piecesId) -> bool:
+    rows = __get_all_like_pieces_id_by_userid(userId)
+    piecesId = int(piecesId)
+    if rows is not None and len(rows) and piecesId in rows:
+        return True
+    return False
+
+
+@bp.route('/pieces/like/add', methods=['GET'])
+def add_pieces_like():
+    """
+    增加pieces的点赞
+    """
+    try:
+        piecesId = request.args.get("piecesId")
+        if piecesId is None:
+            raise NetworkException(400, "前端数据缺失，缺少piecesId")
+
+        user = check_user_before_request(request)
+        userId = user['id']
+        if not __check_if_user_like_pieces(userId, piecesId):
+            # 还没有点赞过
+            __add_pieces_like(userId, piecesId)
+
+        return build_success_response()
+
+    except NetworkException as e:
+        return build_error_response(code=e.code, msg=e.msg)
+
+    except Exception as e:
+        check.printException(e)
+        return build_error_response(code=500, msg='服务器内部错误')
+
+
+def __del_pieces_like(userId, piecesId, trans=None):
+    sql = 'delete from user_pieces_like where userId=%s and piecesId=%s'
+    if trans is None:
+        return execute_sql_write(pooldb, sql, (userId, piecesId))
+    else:
+        return trans.execute(sql, (userId, piecesId))
+
+
+@bp.route('/pieces/like/del', methods=['GET'])
+def del_pieces_like():
+    """
+    取消pieces的点赞
+    """
+    try:
+        piecesId = request.args.get("piecesId")
+        if piecesId is None:
+            raise NetworkException(400, "前端数据缺失，缺少piecesId")
+
+        user = check_user_before_request(request)
+        userId = user['id']
+        __del_pieces_like(userId, piecesId)
+
+        return build_success_response()
+
+    except NetworkException as e:
+        return build_error_response(code=e.code, msg=e.msg)
+
+    except Exception as e:
+        check.printException(e)
+        return build_error_response(code=500, msg='服务器内部错误')
+
+
+@bp.route('/pieces/like/get', methods=['GET'])
+def get_pieces_like():
+    """
+    获取该用户点赞的所有pieces
+    """
+    try:
+        user = check_user_before_request(request)
+        userId = user['id']
+        rows = __get_all_like_pieces_obj_by_userid(userId)
+
+        return build_success_response(data=rows, length=len(rows))
+
+    except NetworkException as e:
+        return build_error_response(code=e.code, msg=e.msg)
+
+    except Exception as e:
+        check.printException(e)
+        return build_error_response(code=500, msg='服务器内部错误')
+
+
+def __add_pieces_collect(userId, piecesId, trans=None):
+    sql = 'insert into user_pieces_collect(userId, piecesId) values(%s, %s)'
+    if trans is None:
+        return execute_sql_write(pooldb, sql, (userId, piecesId))
+    else:
+        return trans.execute(sql, (userId, piecesId))
+
+
+def __get_all_collect_pieces_id_by_userid(userId) -> List[Dict]:
+    """
+    通过用户Id来获取该用户所有collect的pieces的id
+    """
+    sql = 'select piecesId from user_pieces_collect where userId=%s'
+    rows = execute_sql_query(pooldb, sql, userId)
+    rows = list(map(lambda x: int(x['piecesId']), rows))
+    return rows
+
+
+def __get_all_collect_pieces_obj_by_userid(userId) -> List[Dict]:
+    """
+    通过用户Id来获取该用户所有collect的pieces
+    """
+    sql = 'select pieces.piecesId as piecesId, pieces.seriesId as seriesId, ' \
+          'pieces.title as title, pieces.userId as userId, pieces.content as content, ' \
+          'pieces.createTime as createTime, pieces.updateTime as updateTime, ' \
+          'pieces.status as status, pieces.likes as likes, pieces.views as views, ' \
+          'pieces.shares as shares, pieces.collect as collect from pieces, user_pieces_collect ' \
+          'where pieces.piecesId = user_pieces_collect.piecesId ' \
+          'and user_pieces_collect.userId=%s'
+    return execute_sql_query(pooldb, sql, userId)
+
+
+def __check_if_user_collect_pieces(userId, piecesId) -> bool:
+    rows = __get_all_collect_pieces_id_by_userid(userId)
+    piecesId = int(piecesId)
+    if rows is not None and len(rows) and piecesId in rows:
+        return True
+    return False
+
+
+@bp.route('/pieces/collect/add', methods=['GET'])
+def add_pieces_collect():
+    """
+    增加pieces的点赞
+    """
+    try:
+        piecesId = request.args.get("piecesId")
+        if piecesId is None:
+            raise NetworkException(400, "前端数据缺失，缺少piecesId")
+
+        user = check_user_before_request(request)
+        userId = user['id']
+        if not __check_if_user_collect_pieces(userId, piecesId):
+            # 还没有点赞过
+            __add_pieces_collect(userId, piecesId)
+
+        return build_success_response()
+
+    except NetworkException as e:
+        return build_error_response(code=e.code, msg=e.msg)
+
+    except Exception as e:
+        check.printException(e)
+        return build_error_response(code=500, msg='服务器内部错误')
+
+
+def __del_pieces_collect(userId, piecesId, trans=None):
+    sql = 'delete from user_pieces_collect where userId=%s and piecesId=%s'
+    if trans is None:
+        return execute_sql_write(pooldb, sql, (userId, piecesId))
+    else:
+        return trans.execute(sql, (userId, piecesId))
+
+
+@bp.route('/pieces/collect/del', methods=['GET'])
+def del_pieces_collect():
+    """
+    取消pieces的点赞
+    """
+    try:
+        piecesId = request.args.get("piecesId")
+        if piecesId is None:
+            raise NetworkException(400, "前端数据缺失，缺少piecesId")
+
+        user = check_user_before_request(request)
+        userId = user['id']
+        __del_pieces_collect(userId, piecesId)
+
+        return build_success_response()
+
+    except NetworkException as e:
+        return build_error_response(code=e.code, msg=e.msg)
+
+    except Exception as e:
+        check.printException(e)
+        return build_error_response(code=500, msg='服务器内部错误')
+
+
+@bp.route('/pieces/collect/get', methods=['GET'])
+def get_pieces_collect():
+    """
+    获取该用户点赞的所有pieces
+    """
+    try:
+        user = check_user_before_request(request)
+        userId = user['id']
+        rows = __get_all_collect_pieces_obj_by_userid(userId)
+
+        return build_success_response(data=rows, length=len(rows))
+
+    except NetworkException as e:
+        return build_error_response(code=e.code, msg=e.msg)
+
+    except Exception as e:
+        check.printException(e)
+        return build_error_response(code=500, msg='服务器内部错误')
